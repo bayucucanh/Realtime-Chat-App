f93fb1245140ad2d156694093933d211425d9953
dzunn <nurroinz@gmail.com>
2022-05-15 09:32:37 +0700

    create service firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database';
+
+export const login = (email, pass) =>
+  auth().signInWithEmailAndPassword(email, pass);
+
+export const register = (email, pass) =>
+  auth().createUserWithEmailAndPassword(email, pass);
+
+export const forgetPassword = email => auth().sendPasswordResetEmail(email);
+
+export const addUser = (email, username, uid) => {
+  database().ref(`/users/${uid}`).set({
+    email: email,
+    username: username,
+  });
+};
