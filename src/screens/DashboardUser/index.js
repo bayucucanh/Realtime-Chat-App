6552f899767d4b02a6fb0267b2b86abf14830015
Dzun <nurroinz@gmail.com>
2022-05-23 22:17:03 +0700

    update dashboard

--- src/screens/DashboardUser/index.js
+++ src/screens/DashboardUser/index.js
@@ -9,10 +9,13 @@ import {
 import React, {useState, useEffect} from 'react';
 import {COLORS} from '../../themes';
 import database from '@react-native-firebase/database';
+import uuid from 'react-native-uuid';
+import { SearchBar } from '@rneui/base';
 
-export default function DashboardUser() {
+export default function DashboardUser({navigation}) {
   const [allUser, setallUser] = useState([]);
   const [allUserBackup, setallUserBackup] = useState([]);
+  const [search, setsearch] = useState('');
 
   useEffect(() => {
     getAllUser();
@@ -24,24 +27,80 @@ export default function DashboardUser() {
       .once('value')
       .then(snapshot => {
         console.log('all User data: ', Object.values(snapshot.val()));
+        setallUser(Object.values(snapshot.val()));
+        setallUserBackup(Object.values(snapshot.val()));
       });
   };
 
-  const renderItem = ({item}) => {
-    <TouchableOpacity>
-      <View style={styles.card}>
-        <Text style={styles.nameCard}>{item.name}</Text>
-      </View>
-    </TouchableOpacity>;
+  const searchuser = val => {
+    setsearch(val);
+    setallUser(allUserBackup.filter(it => it.name.match(val)));
+  };
+
+  const createChatList = data => {
+    database()
+      .ref('/chatlist/' + userData.id + '/' + data.id)
+      .once('value')
+      .then(snapshot => {
+        console.log('User data: ', snapshot.val());
+
+        if (snapshot.val() == null) {
+          let roomId = uuid.v4();
+          let myData = {
+            roomId,
+            id: userData.id,
+            name: userData.name,
+            emailId: userData.emailId,
+            lastMsg: '',
+          };
+          database()
+            .ref('/chatlist/' + data.id + '/' + userData.id)
+            .update(myData)
+            .then(() => console.log('Data updated.'));
+
+          delete data['password'];
+          data.lastMsg = '';
+          data.roomId = roomId;
+          database()
+            .ref('/chatlist/' + userData.id + '/' + data.id)
+            .update(data)
+            .then(() => console.log('Data updated.'));
+
+          navigation.navigate('ChatScreen', {
+            receiverData: data,
+            userData: userData,
+          });
+        } else {
+          navigation.navigate('ChatScreen', {
+            receiverData: snapshot.val(),
+            userData: userData,
+          });
+        }
+      });
   };
 
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
+      <SearchBar
+        placeholder="Search by name..."
+        onChangeText={val => searchuser(val)}
+        value={search}
+        containerStyle={styles.searchContainer}
+        inputStyle={styles.searchInput}
+      />
       <FlatList
         showsVerticalScrollIndicator={false}
         keyExtractor={(item, index) => index.toString()}
         data={allUser}
-        renderItem={renderItem}
+        renderItem={({item}) => {
+          return (
+            <TouchableOpacity>
+              <View style={styles.card}>
+                <Text style={styles.nameCard}>{item.name}</Text>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
       />
     </SafeAreaView>
   );
@@ -52,7 +111,7 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     width: '90%',
     marginLeft: 5,
-    backgroundColor: COLORS.white,
+    backgroundColor: COLORS.success,
     borderColor: COLORS.green,
     borderRadius: 10,
     justifyContent: 'center',
