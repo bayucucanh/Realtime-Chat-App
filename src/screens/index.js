90e1659d0445629c9b2db2065d65fd05f75895f4
Dzun <nurroinz@gmail.com>
2022-05-23 22:16:51 +0700

    add screen chat

--- src/screens/index.js
+++ src/screens/index.js
@@ -3,5 +3,6 @@ import Login from './Login';
 import Register from './Register';
 import CrudScreen from './CRUD';
 import DashboardUser from './DashboardUser';
+import Chat from './Chat';
 
-export {Splash, Login, Register, CrudScreen, DashboardUser};
+export {Splash, Login, Register, CrudScreen, DashboardUser, Chat};
